adb02b316d2de184c60725e3d6cce5b55108d03f
Philip osir Otieno <philiposir2@gmail.com>
2024-08-04 09:40:32 +0300

    updates

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,36 @@
-import React from "react";
+// import React from "react";
+// import React, { useState } from 'react';
+
+
+
+// function Toggle() {
+
+// const [isOff, setisOff] = useState(true);
+// console.log( isOff);
+
+// function handleClickbutton() {
+//   setisOff((isOff) => !isOff)
+// }
+
+// const color = isOff ? "White" : "Red";
+
+
+//   return <button style={{background: color}} onClick={handleClickbutton}>{isOff ? "OFF" : "ON"}</button>;
+// }
+
+// export default Toggle;
+import React, { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOff, setIsOff] = useState("OFF"); // Initial state is "OFF"
+
+  function handleClickButton() {
+    setIsOff((prevState) => (prevState === "OFF" ? "ON" : "OFF")); // Toggle between "OFF" and "ON"
+  }
+
+  const color = isOff === "OFF" ? "Red" : "White";
+
+  return <button style= {{background: color}} onClick={handleClickButton}>{isOff}</button>;
 }
 
 export default Toggle;
